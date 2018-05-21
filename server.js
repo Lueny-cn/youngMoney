d2760a400eeddeffdaa8c3eab2b791e0d96c1dce
卢文友 <luwenyou@corp.neatease.com>
2018-05-21 16:53:42 +0800

    '[modified]server.js'

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.get('/',(req, res) => {
   res.send(`Hello word, ${port}`)
 })
 
+app.get('/you',(req, res) => {
+  res.send(`Hello lwy, ${port}`)
+})
+
+app.get('/me',(req, res) => {
+  res.send(`Hello hhh, ${port}`)
+})
+
 let port = 8089;
 app.listen(port, ()=>{
   console.log(`server running http://localhost:${port}`);
