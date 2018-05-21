3f64314ef354627213d675f31a8a3a54da0dc945
卢文友 <luwenyou@corp.neatease.com>
2018-05-21 16:36:55 +0800

    '[add]opn'

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import opn from 'opn'
 
 let app = express();
 
@@ -6,7 +7,8 @@ app.get('/',(req, res) => {
   res.send(`Hello word, ${port}`)
 })
 
-let port = 8088;
+let port = 8089;
 app.listen(port, ()=>{
   console.log(`server running http://localhost:${port}`);
+  opn(`http://localhost:${port}`)
 })
